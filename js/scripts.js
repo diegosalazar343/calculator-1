119c4287a0817eb3363d85dbfab7a09b761ffdfa
Duke Skywhopper <yao_shmitz17@yahoo.com>
2020-10-21 17:29:25 -0500

    fix typo in scripts.js file.
    Co-authored-by: Nathan Schrader <nathanschrader@icloud.com>
    
    Co-authored-by: Diego Salazar <salazardiego343@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -16,12 +16,11 @@ const divide = function(number1, number2) {
 
 $(document).ready(function() {
   $("form#add").submit(function(event) {
+    event.preventDefault();
     const number1 = parseInt($("#add1").val());
     const number2 = parseInt($("#add2").val());
     const result = add(number1, number2);
     $("#output").text(result);
-
-   event.preventDefault();
   });
 });
 
